bc2d5c4beff1a1923d5dd7e0cc233d79bba5a3fe
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-10-31 13:44:25 +0200

    Added lesson04 task3

--- lesson04/task3/index.js
+++ lesson04/task3/index.js
@@ -1,1 +1,6 @@
-console.log('Hello, world!');
+for (let i = 1; i <= 5; i++) {
+    console.log('=>' + i + '<=');
+    for (let j = 1; j <= 10; j++) {
+        console.log(i + ' x ' + j + ' = ' + (i * j));
+    }
+}
