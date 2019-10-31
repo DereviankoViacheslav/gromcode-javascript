f7a9e30bae01a4fda98da8f8f465363ace09d8e4
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-10-31 20:15:09 +0200

    Added lesson04 hw3

--- lesson04/hw2/index.js
+++ lesson04/hw2/index.js
@@ -4,14 +4,11 @@ let result = 0;
 
 for (let i = m; i <= n; i++) {
     if ((i % 2 === 0) && (i % 4 !== 0)) {
-        // console.log(i);
         result += i;
     } else if (i % 3 === 0) {
-        // console.log(i);
         result -= i;
     }
     if (i % 4 === 0) {
-        // console.log(i);
         result *= i;
     }
     if (i % 5 === 0) {
--- lesson04/hw3/index.js
+++ lesson04/hw3/index.js
@@ -1,9 +1,15 @@
 let sumOddNumbers = 0;
 
+
 for (let i = 0; i < 1001; i++) {
     if (i % 2 !== 0) {
         console.log('Found');
-        
+        sumOddNumbers += i;
+        if ((sumOddNumbers * 5) > 5000) {
+            console.log('Bigger');
+        } else {
+            console.log('Smaller or equal');
+        }
     }
 }
 
