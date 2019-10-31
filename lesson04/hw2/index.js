5474db4a1f20197e5b0ecc39d6774f7988cc4f45
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-10-31 16:43:22 +0200

    Added lesson04 hw2

--- lesson04/hw2/index.js
+++ lesson04/hw2/index.js
@@ -1,26 +1,19 @@
 const m = 1;
 const n = 50;
-let tmp = 0;
+// let tmp = 0;
 let result = 0;
 
-
 for (let i = m; i <= n; i++) {
+    if (!(i % 2) && (i % 4)) {
+        result += i;
+    } else if (!(i % 3)) {
+        result -= i;
+    } else if (!(i % 4)) {
+        result *= i;
+    }
     if (!(i % 5)) {
         console.log(i);
-        continue;
     }
-    if (!(i % 2) && (i % 4)) {
-        tmp += i;
-    }
-    if (!(i % 3)) {
-        tmp -= i;
-    }
-    if (!(i % 4)) {
-        tmp *= i;
-    }
-    result = tmp;
 }
 
 // console.log(result);
-// console.log(tmp);
-
