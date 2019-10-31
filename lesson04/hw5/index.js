2fff8695d744d11e07f8b0521f7ce2d570767c50
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-10-31 20:46:46 +0200

    Added lesson04 hw5

--- lesson04/hw5/index.js
+++ lesson04/hw5/index.js
@@ -1,1 +1,12 @@
-console.log('Hello, world!');
+let sum = 0;
+
+for (let i = 0; i <= 1000; i++) {
+    sum += i;
+}
+
+const rest = sum % 1234;
+const divisionResult = sum - rest;
+
+let isBigger = divisionResult > rest;
+
+console.log(isBigger);
