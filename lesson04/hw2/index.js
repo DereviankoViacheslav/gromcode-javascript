5835ab8a3f6fea3d5cccfd749a84168a8857b469
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-10-31 15:42:27 +0200

    Added lesson04 hw2

--- lesson04/hw2/index.js
+++ lesson04/hw2/index.js
@@ -5,16 +5,18 @@ let result = 0;
 
 for (let i = m; i <= n; i++) {
     if (!(i % 2) && (i % 4)) {
-        console.log(i + ': число кратно 2 но не кратно 4');
+        // console.log(i + ': число кратно 2 но не кратно 4');
         result += i
     } else if (!(i % 3)) {
-        console.log(i + ': число кратно 3');
+        // console.log(i + ': число кратно 3');
         result -= i
     } else if (!(i % 4)) {
-        console.log(i + ': число кратно 4');
+        // console.log(i + ': число кратно 4');
         result *= i
     } else if (!(i % 5)) {
         console.log(i);
-        console.log(i + ': число кратно 5');
+        // console.log(i + ': число кратно 5');
     }  else continue;
 }
+
+// console.log(result);
