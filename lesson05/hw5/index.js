b7cdfbdb1726141c57ead56caf0d42ca113196ca
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-01 13:49:32 +0200

    Added lesson05 hw5

--- lesson05/hw5/index.js
+++ lesson05/hw5/index.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+function findDivCount(a, b, n) {
+    let cnt = 0;
+    for (let i = a; i < b; i++) {
+        if (i % n) continue;
+        cnt++;
+    }
+    return cnt;
+}
