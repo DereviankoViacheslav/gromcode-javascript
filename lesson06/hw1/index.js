51de61e2e922b558a64f8037056206e2470c7703
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-01 15:33:26 +0200

    Added lesson06 hw1

--- lesson06/hw1/index.js
+++ lesson06/hw1/index.js
@@ -1,1 +1,12 @@
-console.log('Hello, world!');
+function squareElements(array) {
+
+    if (!Array.isArray(array)) return null;
+
+    const result = [];
+
+    for (const item of array) {
+        result.push(item * item);
+    }
+
+    return result;
+}
