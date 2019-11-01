728c0b0c10cb09fc6eccf04572d379ae2415aac9
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-01 11:11:04 +0200

    Added lesson05 hw1

--- lesson05/hw1/index.js
+++ lesson05/hw1/index.js
@@ -1,1 +1,7 @@
-console.log('Hello, world!');
+function getSum(startNum = 0, endNum = 0) {
+    let result = 0;
+    for (let i = startNum; i <= endNum; i++) {
+        if (i % 2 === 0) result += i;
+    }
+    return result;
+}
