08691a776b8659496690e1725d9c6affba85b80e
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-01 16:58:13 +0200

    Added lesson06 hw3

--- lesson06/hw3/index.js
+++ lesson06/hw3/index.js
@@ -1,1 +1,37 @@
-console.log('Hello, world!');
+function sortAsc(arrayNumbers) {
+    if (!Array.isArray(arrayNumbers)) return null;
+
+    const [...array] = arrayNumbers;
+    let max = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] > array[j + 1]) {
+                max = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = max;
+            }
+        }
+    }
+
+    return array;
+}
+
+function sortDesc(arrayNumbers) {
+    if (!Array.isArray(arrayNumbers)) return null;
+    
+    const [...array] = arrayNumbers;
+    let max = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] < array[j + 1]) {
+                max = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = max;
+            }
+        }
+    }
+
+    return array;
+}
