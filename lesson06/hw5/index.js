753a087cf43848a52e52bbab3f826928100d71b5
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-01 18:44:51 +0200

    Added lesson06 hw5

--- lesson06/hw5/index.js
+++ lesson06/hw5/index.js
@@ -1,1 +1,19 @@
-console.log('Hello, world!');
+function removeDuplicates(array) {
+    if (!Array.isArray(array)) return null;
+
+    const uniqueArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        let isIncluding = false;
+        for (let j = 0; j < uniqueArray.length; j++) {
+            if (array[i] === uniqueArray[j]) {
+                isIncluding = true;
+            }
+        }
+        if (!isIncluding) {
+            uniqueArray.push(array[i]);
+        }
+    }
+
+    return uniqueArray;
+}
--- lesson06/hw6/index.js
+++ lesson06/hw6/index.js
@@ -1,1 +1,21 @@
-console.log('Hello, world!');
+function removeDuplicates(array) {
+    if (!Array.isArray(array)) return null;
+
+    const uniqueArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        let isIncluding = false;
+        for (let j = 0; j < uniqueArray.length; j++) {
+            if (array[i] === uniqueArray[j]) {
+                isIncluding = true;
+            }
+        }
+        if (!isIncluding) {
+            uniqueArray.push(array[i]);
+        }
+    }
+
+    return uniqueArray;
+}
+
+console.log(removeDuplicates([1, 2, 3, 3, 5, 5, 8, 9, 0, 4, 5, 9]));
