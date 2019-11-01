c737fd52833a33f5acf424030156797b64dd33af
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-01 15:19:03 +0200

    Added lesson06 task2

--- lesson06/task2/index.js
+++ lesson06/task2/index.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+function getSum(arr) {
+    if (!Array.isArray(arr)) return null;
+    let result = 0;
+    for (const item of arr) {
+        result += item;
+    }
+    return result;
+}
