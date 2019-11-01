643c298bf71a178f04756474de6271a6d1509458
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-01 21:19:04 +0200

    Added lesson06 hw6

--- lesson06/hw6/index.js
+++ lesson06/hw6/index.js
@@ -1,15 +1,14 @@
+// const clients = ['Jeck0', 'Jeck1', 'Jeck2', 'Jeck3', 'Jeck4', 'Jeck5', 'Jeck6'];
+// const balances = [100, 200, 300, 400, 500, 600, 700];
 
-const clients = ['Jeck0', 'Jeck1', 'Jeck2', 'Jeck3', 'Jeck4', 'Jeck5', 'Jeck6'];
-const balances = [100, 200, 300, 400, 500, 600, 700];
-
-console.log(withdraw(clients, balances, 2, 50));
+// console.log(withdraw(clients, balances, 2, 500));
 
 function withdraw(clients, balances, client, amount) {
 
-    if (balances[client] > amount) {
+    if (balances[client] < amount) {
+        return -1;
+    } else {
         balances[client] -= amount;
         return balances[client];
-    } else {
-        return -1;
     }
 }
