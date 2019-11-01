e23539cf13cf98cc48219581100c6a9955bc2a15
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-01 13:42:59 +0200

    Added lesson05 hw4

--- lesson05/hw4/index.js
+++ lesson05/hw4/index.js
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+function compareSums(a, b, c, d) {
+    if (sum(a, b) > sum(c, d)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function sum(from, to) {
+    let result = 0;
+    for (let i = from; i <= to; i++) {
+        result += i;
+    }
+    return result;
+}
