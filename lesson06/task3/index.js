7967e417b6c96d632f61764435a442a034c96043
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-01 15:24:41 +0200

    Added lesson06 task3

--- lesson06/task3/index.js
+++ lesson06/task3/index.js
@@ -1,1 +1,11 @@
-console.log('Hello, world!');
+function getSpecialNumbers(from, to) {
+    let array = [];
+
+    for (let i = from; i < to; i++) {
+        if (i % 3 === 0) {
+            array.push(i);
+        }
+    }
+
+    return array;
+}
