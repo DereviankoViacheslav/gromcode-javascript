b628bb05363735d595f588e2dff6049e66b8d039
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-01 21:30:25 +0200

    Added lesson06 hw7

--- lesson06/hw7/index.js
+++ lesson06/hw7/index.js
@@ -1,1 +1,18 @@
-console.log('Hello, world!');
+function checker(array) {
+    let max = array[0];
+    let min = array[0];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    if (max + min > 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
