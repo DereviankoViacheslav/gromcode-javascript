9d71394d66dea780df7a3b66771dbf54bd7ca8d7
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-04 16:42:24 +0200

    Added lesson08 hw2

--- lesson08/hw2/index.js
+++ lesson08/hw2/index.js
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+const getAdults = obj => {
+    const resultObj = {};
+    for (const key in obj) {
+        if (+obj[key] >= 18) {
+            resultObj[key] = obj[key];
+        }
+    }
+    return resultObj;
+}
