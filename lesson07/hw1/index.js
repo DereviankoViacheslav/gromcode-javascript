4ae9c4c8712ba9f7227c3352def190ba75686b7a
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-04 14:35:11 +0200

    Added lesson07 hw1

--- lesson07/hw1/index.js
+++ lesson07/hw1/index.js
@@ -1,11 +1,4 @@
-
-
-
-
-const allStudents = ['a', 'b', 'c', 'd'];
-const fialedStudents = ['c', 'd'];
-
-
-const array = [2, 5, [9, 8, 7, 4], 5, 6, 8];
-console.log(getMessagesForBestStudents(allStudents, fialedStudents));
-console.log(allStudents, fialedStudents);
+const squareArray = array => {
+    if (!Array.isArray(array)) return null;
+    return array.map(elem => elem * elem);
+}
