0244ffbef437c6b4f42f41a4039d3975d9a6075d
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-04 16:28:23 +0200

    Added lesson08 hw1

--- lesson08/hw1/index.js
+++ lesson08/hw1/index.js
@@ -1,1 +1,11 @@
-console.log('Hello, world!');
+const pickProps = (obj, array) => {
+    return array.reduce((acc, elem) => {
+        for (const key in obj) {
+            if (key === elem) {
+                acc[elem] = obj[key];
+                break;
+            }
+        }
+        return acc;
+    }, {});
+}
