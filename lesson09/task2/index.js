df962f83b061a0a0ef53f0a1b6d5d7a00e20ede2
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-04 22:32:45 +0200

    Added lesson09 task2

--- lesson09/task2/index.js
+++ lesson09/task2/index.js
@@ -1,1 +1,12 @@
-const copyObj = obj => resultObj = { ...obj };
+const copyObj = obj => Object.assign({}, obj);
+
+// const copyObj2 = obj => obj;
+
+// const obj = {
+//     'John Doe': 19,
+//     'Tom': 17,
+//     'Bob': 18
+// };
+
+// console.log(copyObj(obj) === obj);
+// console.log(copyObj2(obj) === obj);
