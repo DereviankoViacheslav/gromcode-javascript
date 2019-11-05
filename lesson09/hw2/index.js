6dc60aec5bb815440d5d783f3d03ac0a562d6aad
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-05 12:37:14 +0200

    Added lesson09 hw2

--- lesson09/hw2/index.js
+++ lesson09/hw2/index.js
@@ -1,22 +1,22 @@
-const getPeople = obj => {
-    
-}
+const getPeople = obj => Object.entries(obj)
+    .map(elem => elem[1].map(elem => elem.name))
+    .flat();
 
-const customers = {
-    'customer-id-1': {
-        name: 'William',
-        age: 54
-    },
-    'customer-id-2': {
-        name: 'Tom',
-        age: 17
-    },
-};
+// const rooms = {
+//     room1: [
+//         { name: 'room1 name1' },
+//         { name: 'room1 name2' },
+//         { name: 'room1 name3' },
+//         { name: 'room1 name4' },
+//     ],
+//     room2: [
+//         { name: 'room2 name1' },
+//     ],
+//     room3: [
+//         { name: 'room3 name1' },
+//         { name: 'room3 name2' },
+//         { name: 'room3 name3' },
+//     ],
+// };
 
-/*Нужно написать функцию, которая такой объект преобразует в массив имен жителей
-
-Основные требования:
-Функция с именем getPeople должна принимать объект в заданом выше формате
-Функция должна вернуть список имен пользователей в виде массива (плоский массив строк ['room1 name1', 'room1 name2', ... 'room2 name1', 'room3 name1', ...]).
-Количество комнат и количество людей в комнате может быть любым.
-Пользуйтесь разнообразием методов массивов и объектов. Обычный цикл for для решения этой задачи подходит не лучшим образом. */
+// console.log(getPeople(rooms));
