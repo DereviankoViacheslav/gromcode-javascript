338ce74b19b17fb1b17311a3245b9e28111d6be5
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-05 21:33:45 +0200

    Added lesson10 task1

---
+++ lesson10/task1/index.js
@@ -0,0 +1,23 @@
+const getFiniteNumbers = array => array.filter(el => Number.isFinite(el));
+
+const getFiniteNumbersV2 = array => array.filter(el => isFinite(el));
+
+const getNaN = array => array.filter(el => !Number.isNaN(el));
+
+const getNaNV2 = array => array.filter(el => !isNaN(el));
+
+const getIntegers = array => array.filter(el => Number.isInteger(el));
+
+// const arr = [2, 34, 56, 78, '35', true, undefined, 98, 6, NaN, 54, 75, 34, Infinity, -Infinity];
+
+// console.log(getFiniteNumbers(arr));
+// console.log(getFiniteNumbersV2(arr));
+// console.log(getNaN(arr));
+// console.log(getNaNV2(arr));
+// console.log(getIntegers(arr));
+// console.log(arr);
+
+
+/*Функция с названием getIntegers должна отбросить все элементы исходного массива, которые не являются целыми числами. Для проверки должна использовать метод
+Во всех ф-циях исходный массив должен оставаться неизменным
+Сравните результаты работы методов Number.isNaN vs isNaN и Number.isFinite vs isFinite */
