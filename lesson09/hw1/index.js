6657320a7f001ce44c969157ec2a61203d99b701
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-05 09:36:04 +0200

    Added lesson09 hw1

--- lesson09/hw1/index.js
+++ lesson09/hw1/index.js
@@ -1,1 +1,19 @@
-console.log('Hello, world!');
+const getCustomersList = obj => {
+    for (const key in obj) {
+        obj[key]['id'] = key;
+    }
+    return Object.values(obj).sort((elem1, elem2) => elem1.age - elem2.age);
+}
+
+// const customers = {
+//     'customer-id-1': {
+//         name: 'William',
+//         age: 54
+//     },
+//     'customer-id-2': {
+//         name: 'Tom',
+//         age: 17
+//     },
+// };
+
+// console.log(getCustomersList(customers));
--- lesson09/task2/index.js
+++ lesson09/task2/index.js
@@ -1,12 +1,1 @@
 const copyObj = obj => Object.assign({}, obj);
-
-// const copyObj2 = obj => obj;
-
-// const obj = {
-//     'John Doe': 19,
-//     'Tom': 17,
-//     'Bob': 18
-// };
-
-// console.log(copyObj(obj) === obj);
-// console.log(copyObj2(obj) === obj);
