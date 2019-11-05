d0024aaf40c047ddd6ec43a00960c2c9d4f1fd71
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-05 11:53:11 +0200

    Added lesson09 hw1

--- lesson09/hw1/index.js
+++ lesson09/hw1/index.js
@@ -1,11 +1,15 @@
-const getCustomersList = obj => {
-    const resultObj = {};
-    for (const key in obj) {
-        resultObj[key] = {...obj[key]}
-        resultObj[key]['id'] = key;
-    }
-    return Object.values(resultObj).sort((elem1, elem2) => elem1.age - elem2.age);
-}
+// const getCustomersList = obj => {
+//     const resultObj = {};
+//     for (const key in obj) {
+//         resultObj[key] = {...obj[key]}
+//         resultObj[key]['id'] = key;
+//     }
+//     return Object.values(resultObj).sort((elem1, elem2) => elem1.age - elem2.age);
+// }
+
+const getCustomersList = obj => Object.entries(obj)
+    .map(elem => ({id: elem[0], ...obj[elem[0]]}))
+    .sort((elem1, elem2) => elem1.age - elem2.age);
 
 // const customers = {
 //     'customer-id-1': {
