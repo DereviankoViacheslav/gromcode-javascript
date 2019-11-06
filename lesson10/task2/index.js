b8b8b08d54823afb501ae286e7c862e5b3a2fc5c
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-06 07:58:34 +0200

    Added lesson10 task2

--- lesson10/task2/index.js
+++ lesson10/task2/index.js
@@ -1,9 +1,9 @@
-const getParsedIntegers = array=> array.map(el => Number.parseInt(el));
+const getParsedIntegers = array => array.map(el => Number.parseInt(el));
 
-const getParsedIntegersV2 = array=> array.map(el => parseInt(el));
+const getParsedIntegersV2 = array => array.map(el => parseInt(el));
 
-const getParsedFloats = array=> array.map(el => Number.parseFloat(el));
+const getParsedFloats = array => array.map(el => Number.parseFloat(el));
 
-const getParsedFloatsV2 = array=> array.map(el => parseFloat(el));
+const getParsedFloatsV2 = array => array.map(el => parseFloat(el));
 
-const getParsedFloats = array=> array.map(el => Number.parseFloat(el));
+const getParsedFloats = array => array.map(el => Number.parseFloat(el));
