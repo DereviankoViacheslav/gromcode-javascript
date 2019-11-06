9f20a9d0d43ae419d177b435c4caebe62df90c47
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-06 15:39:23 +0200

    Added lesson11 task1

--- lesson11/task1/index.js
+++ lesson11/task1/index.js
@@ -1,1 +1,16 @@
-console.log('Hello, world!');
+const splitText = (str, length = 10) => {
+
+    if (typeof str !== 'string') return null;
+
+    const resultArray = [];
+    let startPosition = 0;
+
+    while (true) {
+        let chank = str.substr(startPosition, length);
+        if (chank.length === 0) break;
+        resultArray.push(chank[0].toUpperCase() + chank.slice(1));
+        startPosition += length;
+    }
+
+    return resultArray.join('\n');
+}
