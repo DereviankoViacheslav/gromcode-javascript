5118ca4c9dace904f3fb1285a58b34172f5843fb
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-06 15:52:17 +0200

    Added lesson11 task2

--- lesson11/task2/index.js
+++ lesson11/task2/index.js
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+const sortContacts = (contactsArray, isAsc) => {
+    return contactsArray.sort((a, b) => {
+        if (isAsc) {
+            return a.name.localeCompare(b.name);
+        } else {
+            return b.name.localeCompare(a.name);
+        }
+    });
+}
