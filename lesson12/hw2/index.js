d7a6d1f19a7228a7fa19a52b4e1affb1ddd5ab2a
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-06 18:13:52 +0200

    Added lesson12 hw2

--- lesson12/hw2/index.js
+++ lesson12/hw2/index.js
@@ -1,1 +1,4 @@
-console.log('Hello, world!');
+const cleanTransactionsList = array => {
+    return array.filter(el => typeof (+el) === 'number' && !isNaN(el))
+        .map(el => `$${(+el).toFixed(2)}`);
+}
