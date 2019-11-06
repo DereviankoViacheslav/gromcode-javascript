eee256c9036c02a3a31e2ab6e46610839e50400c
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-06 11:08:54 +0200

    Added lesson10 hw2

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -2,9 +2,9 @@ const getRandomNumbers = (arrayLength, from, to) => {
     if (from > to || (to - from) < 1) return null;
     const array = [];
     for (let i = 0; i < arrayLength; i++) {
-        array.push(Math.round((Math.random() * (to - from) + from + 1)));
+        array.push(Math.floor((Math.random() * (to - from) + from)));
     }
     return array;
 }
 
-// console.log(getRandomNumbers(20, -5.568, 10));
+// console.log(getRandomNumbers(20, -5.568, 10.999));
