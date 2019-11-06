3469ea7aa9d62fa6ce5f4cb9da9f132eff183ae7
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-06 11:13:19 +0200

    Added lesson10 hw2

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -1,8 +1,10 @@
 const getRandomNumbers = (arrayLength, from, to) => {
     if (from > to || (to - from) < 1) return null;
+    const min = Math.ceil(from);
+    const max = Math.floor(to);
     const array = [];
     for (let i = 0; i < arrayLength; i++) {
-        array.push(Math.floor((Math.random() * (to - from) + from)));
+        array.push(Math.trunc((Math.random() * (max - min) + min + 1)));
     }
     return array;
 }
