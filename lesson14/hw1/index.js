098e0814e9ab5316425438bb8e56a27e98a243c0
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-09 15:01:35 +0200

    Added lesson14 hw1

--- lesson14/hw1/index.js
+++ lesson14/hw1/index.js
@@ -1,1 +1,17 @@
-console.log('Hello, world!');
+let result = 0;
+
+export function add(num) {
+    result += num;
+}
+
+export function decrease(num) {
+    result -= num;
+}
+
+export function reset() {
+    result = 0;
+}
+
+export function getMemo() {
+    return result;
+}
