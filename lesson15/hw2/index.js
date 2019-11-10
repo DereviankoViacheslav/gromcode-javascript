966b976a57586a39320abc4a1adca9079a70c39d
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-11-10 13:55:20 +0200

    Added lesson15 hw2

--- lesson15/hw2/index.js
+++ lesson15/hw2/index.js
@@ -1,16 +1,36 @@
+function createLogger() {
 
+    const logger = [];
 
-/*Напишите ф-цию, которая будет создавать логгеры
+    return {
+        warn: function(text) {
+            logger.push({
+                message: text,
+                dateTime: new Date(),
+                type: 'warn',
+            });
+        },
+        error: function(text) {
+            logger.push({
+                message: text,
+                dateTime: new Date(),
+                type: 'error',
+            });
+        },
+        log: function(text) {
+            logger.push({
+                message: text,
+                dateTime: new Date(),
+                type: 'log',
+            });
+        },
+        getRecords: function(type) {
+            const resultArray = type !== undefined ? logger.filter(el => el.type === type): logger;
+            return resultArray.sort((a, b) => {
+                return b.dateTime.getTime() - a.dateTime.getTime();
+            });;
+        },
+    };
+}
 
-Основные требования
-Из файла index.js должна экспортироваться ф-ция с именем createLogger
-createLogger должна вернуть логгер с методами:
-warn - принимает ворнинг в виде строки и записывает в память
-error - принимает ошибку в виде строки и записывает в память
-log - принимает сообщение в виде строки и записывает в память
-getRecords - принимает строку 'warn' или 'error' или 'log' и возвращает соответственно ворнинги, ошибки или логи в формате { message: [сохраненное сообщение], dateTime: [время регистрации сообщения], type: [тип записи] }, где type - 'warn' или 'error' или 'log'
-getRecords возвращает все сообщения (всех типов), если аргумент не передан
-getRecords должен вернуть сообщения в виде массива, отсортированному по времени по убыванию (новые сообщения в массиве идут первыми)
-getRecords должен возвращать пустые массивы, если сообщений соответствующего типа не поступало
-Логгеры, созданные с помощью createLogger должны быть независимы друг от друга (у каждого своя память)
-Обязательно покрывайте и отлаживайте ваше решение тестами перед проверкой на сайте*/
+export { createLogger };
