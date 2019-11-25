4cfb0b62eee934183b737225b12720316b7b53e7
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-11-25 22:50:58 +0200

    add lesson18 task1

--- lesson18/task1/index.js
+++ lesson18/task1/index.js
@@ -1,1 +1,33 @@
-console.log('Hello, world!');
+const event = {
+    guests: [{
+            name: 'John',
+            age: 17,
+            email: 'example1@server.com'
+        },
+        {
+            name: 'Bob',
+            age: 16,
+            email: 'example2@server.com'
+        },
+        {
+            name: 'Nick',
+            age: 18,
+            email: 'example3@server.com'
+        },
+    ],
+    message: 'Welcome to the party!',
+    getInvitations() {
+        return this.guests
+        .filter(({ age }) => age >= 18)
+        .map(({ name, email }) => {
+            return {
+                email,
+                message: `Dear ${this.name}! ${this.message}`,
+            };
+        });
+    },
+};
+
+export {
+    event
+};
