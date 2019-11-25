fd628923c4f459e89d6c90a9416d5d538137ae74
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-11-26 00:01:49 +0200

    add lesson18 hw1

--- lesson18/hw1/index.js
+++ lesson18/hw1/index.js
@@ -1,1 +1,11 @@
-console.log('Hello, world!');
+function spy(func) {
+    fn.calls = [];
+    function fn(...args) {
+        fn.calls.push(args);
+        return func.apply(this, args);
+    }
+
+    return fn;
+}
+
+export { spy };
