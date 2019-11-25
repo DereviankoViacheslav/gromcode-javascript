7be3d9215aa6526c0226bc3be42c8fc4ee0b926a
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-11-25 22:58:21 +0200

    add lesson18 task2

--- lesson18/task2/index.js
+++ lesson18/task2/index.js
@@ -1,1 +1,11 @@
-console.log('Hello, world!');
+const wallet = {
+    transactions: [100, 200, 300],
+    getMax() {
+        return Math.max(...this.transactions);
+    },
+    getMin() {
+        return Math.min(...this.transactions);
+    },
+};
+
+export { wallet };
