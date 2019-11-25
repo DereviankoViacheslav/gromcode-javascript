2030b91b18eefe89d0dfe28ec10ce0a85cce5c5c
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-11-25 12:43:33 +0200

    add lesson17 hw1

--- lesson17/hw1/index.js
+++ lesson17/hw1/index.js
@@ -1,1 +1,14 @@
-console.log('Hello, world!');
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    getFullName() {
+        return this.firstName + ' ' + this.lastName;
+    },
+    setFullName(fullName) {
+        let [first, last] = fullName.split(' ');
+        this.firstName = first;
+        this.lastName = last;
+    },
+}
+
+export { user };
