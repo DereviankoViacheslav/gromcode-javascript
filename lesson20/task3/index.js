38ad08feb9d8a5854ae5149fc45ceb1c9c5f6b34
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-11-26 17:37:37 +0200

    add lesson20 task3

--- lesson20/task3/index.js
+++ lesson20/task3/index.js
@@ -1,1 +1,23 @@
-console.log('Hello, world!');
+class Wallet {
+    balance = 0;
+
+    getBalance() {
+        return this.balance;
+    }
+
+    deposit(value) {
+        this.balance += value;
+    }
+
+    withdraw(value) {
+        if(value < this.balance) {
+            console.log('No enough funds');
+            return;
+        }
+        this.balance -= value;
+    }
+};
+
+export {
+    Wallet
+};
