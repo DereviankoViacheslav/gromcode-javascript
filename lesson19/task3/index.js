a74b015bce5ee0e79d33aa2939cb761656f85cde
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-11-26 14:53:00 +0200

    add lesson19 task4

--- lesson19/task3/index.js
+++ lesson19/task3/index.js
@@ -1,16 +1,16 @@
-const user = {
-    firstName: 'John',
-    lastName: 'Doe',
+function User(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
     get fullName() {
         return `${this.firstName} ${this.lastName}`;
-    },
+    };
     set fullName(fullName) {
         const [firstName, lastName] = fullName.split(' ');
         this.firstName = firstName;
         this.lastName = lastName;
-    },
+    };
 }
 
 export {
-    user
+    User
 };
