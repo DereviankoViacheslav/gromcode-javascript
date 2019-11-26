8500eba628214c57e19a054393be46690d4dd381
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-11-26 08:48:29 +0200

    add lesson17 hw3

--- lesson17/hw3/index.js
+++ lesson17/hw3/index.js
@@ -1,7 +1,5 @@
 function bind(func, context, ...args) {
-    // let bindArgs = [].slice.call(arguments, 2);
     return function(...fnArgs) {
-        // let fnArgs = [].slice.call(arguments);
         return func.call(context, ...args, ...fnArgs);
       };
 }
