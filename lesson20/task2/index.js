e1883ba5966e9c39c50e01d3e57b26d64b57107f
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-11-26 17:17:50 +0200

    add lesson20 task2

--- lesson20/task2/index.js
+++ lesson20/task2/index.js
@@ -1,1 +1,38 @@
-console.log('Hello, world!');
+class Vehicle {
+
+    constructor(name, numberOfWheels) {
+        this.name = name;
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    move() {
+        console.log(`${this.name} is moving`);
+    }
+
+    stop() {
+        console.log(`${this.name} stopped`);
+    }
+};
+
+class Ship extends Vehicle {
+
+    constructor(name, numberOfWheels, maxSpeed) {
+        super(name, numberOfWheels);
+        this.maxSpeed = maxSpeed;
+    }
+
+    start() {
+        console.log(`${this.name} lifting anchor up`);
+        this.move();
+    }
+
+    stop() {
+        super.stop(),
+        console.log(`${this.name} lifting anchor down`);
+    }
+};
+
+export {
+    Vehicle,
+    Ship
+};
