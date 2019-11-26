4b544fde77494b2ebc6f134aec56bad59355d51f
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-11-26 14:06:16 +0200

    add lesson19 task2

--- lesson19/task2/index.js
+++ lesson19/task2/index.js
@@ -1,1 +1,14 @@
-console.log('Hello, world!');
+function getOwnProps(obj) {
+    const arrOwnPropsObj = [];
+    for (const prop in obj) {
+        if (obj.hasOwnProperty(prop) && typeof prop !== 'function') {
+            arrOwnPropsObj.push(prop);
+        }
+    }
+
+    return arrOwnPropsObj;
+}
+
+export {
+    getOwnProps
+};
