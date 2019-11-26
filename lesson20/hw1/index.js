98c27413530c27be77d432e400604587279a31ec
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-11-26 18:18:08 +0200

    add lesson20 hw1

--- lesson20/hw1/index.js
+++ lesson20/hw1/index.js
@@ -1,1 +1,29 @@
-console.log('Hello, world!');
+class Order {
+
+    constructor(price, dateCreated, isConfirmed, dateConfirmed, city, type) {
+        this.price = price;
+        this.dateCreated = dateCreated;
+        this.isConfirmed = isConfirmed;
+        this.dateConfirmed = dateConfirmed;
+        this.city = city;
+        this.type = type;
+        this.id = `${Math.random()}`;
+    }
+
+    checkPrice() {
+        if(this.price < 1000) return false;
+        return true;
+    }
+
+    confirmOrder() {
+        this.isConfirmed = true;
+        this.dateConfirmed = new Date();
+    }
+
+    isValidType() {
+        if(this.type === 'Buy' || this.type === 'Sale') return true;
+        return false;
+    }
+};
+
+export { Order };
