74b9dbfa2f7bffd65dc2d4d0544ee668f3a47e1f
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-11-27 09:05:35 +0200

    add lesson20 hw2

--- lesson20/hw2/index.js
+++ lesson20/hw2/index.js
@@ -1,29 +1,29 @@
 class User {
 
     constructor(id, name, sessionId) {
-        this.id = id;
-        this.name = name;
-        this.sessionId = sessionId;
+        this._id = id;
+        this._name = name;
+        this._sessionId = sessionId;
     }
 };
 
 class UserRepository {
 
     constructor(users) {
-        this.users = Object.freeze(users);
+        this._users = Object.freeze(users);
     }
 
     getUserNames() {
-        return this.users.map(({ name }) => name);
+        return this._users.map(({ _name }) => _name);
     }
 
     getUserIds() {
-        return this.users.map(({ id }) => id);
+        return this._users.map(({ _id }) => _id);
     }
     
     getUserNameById(userId) {
-        const user = this.users.find(({ id }) => id === userId);
-        return user ? user.name : user;
+        const user = this._users.find(({ _id }) => _id === userId);
+        return user ? user._name : user;
     }
 };
 
