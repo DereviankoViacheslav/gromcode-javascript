70ddf00a43cdce42365d4bb18b9ebdc001035c5f
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-11-27 11:02:01 +0200

    add lesson20 hw2

--- lesson20/hw2/index.js
+++ lesson20/hw2/index.js
@@ -1,9 +1,9 @@
 export class User {
 
     constructor(id, name, sessionId) {
-        this.id = id;
-        this.name = name;
-        this.sessionId = sessionId;
+        this._id = id;
+        this._name = name;
+        this._sessionId = sessionId;
     }
 };
 
@@ -26,3 +26,20 @@ export class UserRepository {
         return user ? user.name : user;
     }
 };
+
+// const arr = [
+//     new User(1, 'aaa', 10001),
+//     new User(2, 'bbb', 10002),
+//     new User(3, 'ccc', 10003),
+//     new User(4, 'ddd', 10004),
+//     new User(5, 'eee', 10005),
+// ];
+
+// const Users = new UserRepository(arr);
+
+// console.log(Users._users);
+// Users._users.push(new User(6, 'fff', 19996));
+// Users._users = 'str';
+// console.log(Users._users);
+
+// export { User, UserRepository };
