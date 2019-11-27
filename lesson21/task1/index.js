fb7f4ba03ded81e52bcfb8e5a25bac04ae964a2f
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-11-27 18:29:20 +0200

    add lesson21 task1

--- lesson21/task1/index.js
+++ lesson21/task1/index.js
@@ -1,60 +1,29 @@
-// const tasks = [
-//     { text: 'Buy milk', done: false },
-//     { text: 'Pick up Tom from airport', done: false },
-//     { text: 'Visit party', done: false },
-//     { text: 'Visit doctor', done: true },
-//     { text: 'Buy meat', done: true },
-// ];
-
-// const renderList = (tasks) => {
-//     const tasksList = document.querySelector('.list');
-//     const listCreatedTasks = tasks
-//     .sort((a, b) => a.done - b.done)
-//     .map(({ text, done}) => {
-//         const listItem = document.createElement('li');
-//         listItem.classList.add('list__item');
-//         const chBox = document.createElement('input');
-//         chBox.setAttribute('type', 'checkbox');
-//         chBox.checked = done;
-//         if(done) listItem.classList.add('list__item_done');
-//         chBox.classList.add('list__item-checkbox');
-//         listItem.append(chBox, text);
-
-//         return listItem;
-//     });
-
-//     tasksList.append(...listCreatedTasks);
-// };
-
-// renderList(tasks);
-
-const task = [
+const tasks = [
     { text: 'Buy milk', done: false },
     { text: 'Pick up Tom from airport', done: false },
     { text: 'Visit party', done: false },
     { text: 'Visit doctor', done: true },
     { text: 'Buy meat', done: true },
-]
+];
 
-const rederListItem = listItem => {
+const renderList = (tasks) => {
+    const tasksList = document.querySelector('.list');
+    const listCreatedTasks = tasks
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done}) => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('list__item');
+        const chBox = document.createElement('input');
+        chBox.setAttribute('type', 'checkbox');
+        chBox.checked = done;
+        if(done) listItem.classList.add('list__item_done');
+        chBox.classList.add('list__item-checkbox');
+        listItem.append(chBox, text);
 
-    const listElement = document.querySelector('.list');
-    const listItemElements = listItem
-        .sort((a, b) => a.done - b.done)
-        .map(({ text, done }) => {
-            const listItemElem = document.createElement('li')
-            listItemElem.classList.add('list__item');
-            const checkboxElem = document.createElement('input');
-            checkboxElem.setAttribute('type', 'checkbox')
-            checkboxElem.checked = done;
-            if (done) listItemElem.classList.add('list__item_done')
-            checkboxElem.classList.add('list__item-checkbox')
-            listItemElem.append(checkboxElem, text)
-            return listItemElem
-        });
+        return listItem;
+    });
 
-    listElement.append(...listItemElements)
-}
+    tasksList.append(...listCreatedTasks);
+};
 
-
-rederListItem(task);
+renderList(tasks);
