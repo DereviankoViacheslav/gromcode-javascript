2e5e224f7d0e56683631adea3b8d9cae3dbd02bf
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-01 15:31:45 +0200

    add lesson24 task1

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,1 +1,10 @@
-console.log('Hello, world!');
+function dayOfWeek(date, days) {
+    const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const day = new Date(date).getDate();
+    const dateInFuture = new Date(date).setDate(day + days);
+    return weekDays[new Date(dateInFuture).getDay()];
+}
+
+export {
+    dayOfWeek
+};
