f4e25cdb9a67c73ac057805bc64bf082d3ef4829
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-02 11:26:02 +0200

    add lesson24 hw2

--- lesson24/hw2/index.js
+++ lesson24/hw2/index.js
@@ -1,21 +1,29 @@
 function getDiff(startDate, endDate) {
-    const diffDates = Math.abs(Date.parse(endDate) - Date.parse(startDate));
-    const SECONDS_PER_MINUTE = 60 * 1000;
-    const SECONDS_PER_HOUR = 60 * SECONDS_PER_MINUTE;
-    const SECONDS_PER_DAY = 24 * SECONDS_PER_HOUR;
-    const cntDays = Math.trunc(diffDates / SECONDS_PER_DAY);
-    const cntHours = Math.trunc((
-        diffDates - (cntDays * SECONDS_PER_DAY)) / SECONDS_PER_HOUR);
-    const cntMinutes = Math.trunc((
-        diffDates - (cntDays * SECONDS_PER_DAY) - 
-        (cntHours * SECONDS_PER_HOUR)) / SECONDS_PER_MINUTE);
-    const cntSeconds = Math.trunc((
-            diffDates - (cntDays * SECONDS_PER_DAY) -
-            (cntHours * SECONDS_PER_HOUR) -
-            (cntMinutes * SECONDS_PER_MINUTE)) / 1000);
-    
-    return `${cntDays}d ${cntHours}h ${cntMinutes}m ${cntSeconds}s`;
+    const diffInMilliseconds = Math.abs(Date.parse(endDate) - Date.parse(startDate));
+
+    const MILLISECONDS_PER_MINUTE = 60 * 1000;
+    const MILLISECONDS_PER_HOUR = 60 * MILLISECONDS_PER_MINUTE;
+    const MILLISECONDS_PER_DAY = 24 * MILLISECONDS_PER_HOUR;
+
+    const countDays = Math.trunc(diffInMilliseconds / MILLISECONDS_PER_DAY);
+    const countDaysInMilliseconds = countDays * MILLISECONDS_PER_DAY;
+
+    const restOfDayInMilliseconds = diffInMilliseconds - countDaysInMilliseconds;
+
+    const countHours = Math.trunc((restOfDayInMilliseconds) / MILLISECONDS_PER_HOUR);
+    const restOfHourInMilliseconds = restOfDayInMilliseconds - (countHours * MILLISECONDS_PER_HOUR);
+
+    const countMinutes = Math.trunc((restOfHourInMilliseconds) / MILLISECONDS_PER_MINUTE);
+    const countSeconds = Math.trunc(
+        (restOfHourInMilliseconds - (countMinutes * MILLISECONDS_PER_MINUTE)) / 1000);
+
+    return `${countDays}d ${countHours}h ${countMinutes}m ${countSeconds}s`;
 }
+
+// const start = new Date(2005, 5, 6, 7, 55, 10);
+// const end = new Date(2006, 1, 7, 8, 15, 45);
+
+// console.log(getDiff(start, end));
 
 export {
     getDiff
