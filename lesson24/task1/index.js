ca9e04709457b09b801cae96b549b9d744794314
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-02 11:41:37 +0200

    add lesson24 task1

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,5 +1,5 @@
 function dayOfWeek(date, days) {
-    const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const weekDays = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
     const day = new Date(date).getDate();
     const dateInFuture = new Date(date).setDate(day + days);
     return weekDays[new Date(dateInFuture).getDay()];
