18eda0c3f8c911e53518ec084ccbc20b3c4224dd
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-02 12:03:42 +0200

    add lesson24 hw1

--- lesson24/hw1/index.js
+++ lesson24/hw1/index.js
@@ -1,1 +1,29 @@
-console.log('Hello, world!');
+function getDiff(startDate, endDate) {
+    const diffMs = Math.abs(endDate - startDate);
+
+    const MILLSEC_PER_MINUTE = 60 * 1000;
+    const MILLSEC_PER_HOUR = 60 * MILLSEC_PER_MINUTE;
+    const MILLSEC_PER_DAY = 24 * MILLSEC_PER_HOUR;
+
+    const countDays = Math.trunc(diffMs / MILLSEC_PER_DAY);
+    const countDaysInMs = countDays * MILLSEC_PER_DAY;
+
+    const restOfDayInMs = diffMs - countDaysInMs;
+
+    const countHours = Math.trunc((restOfDayInMs) / MILLSEC_PER_HOUR);
+    const restOfHourInMs = restOfDayInMs - (countHours * MILLSEC_PER_HOUR);
+
+    const countMinutes = Math.trunc((restOfHourInMs) / MILLSEC_PER_MINUTE);
+    const countSeconds = Math.trunc((restOfHourInMs - (countMinutes * MILLSEC_PER_MINUTE)) / 1000);
+
+    return `${countDays}d ${countHours}h ${countMinutes}m ${countSeconds}s`;
+}
+
+// const start = new Date(2005, 5, 6, 7, 55, 10);
+// const end = new Date(2006, 1, 7, 8, 15, 45);
+
+// console.log(getDiff(start, end));
+
+export {
+    getDiff
+};
