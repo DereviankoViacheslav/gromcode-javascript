68615767bd4b572941bf8e5adee49445bcd4e656
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-02 17:09:27 +0200

    add lesson24 hw2

--- lesson24/hw2/index.js
+++ lesson24/hw2/index.js
@@ -6,60 +6,47 @@ function studentsBirthDays(students) {
     const arrStudents = students.slice();
     const result = {};
     arrStudents
-        .sort((a, b) => new Date(a.birthDay).getDate() - new Date(b.birthDay).getDate())
-        // .sort((a, b) => new Date(a.birthDay) - new Date(b.birthDay))
+        .sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate())
+        // .sort((a, b) => new Date(a.birthDate) - new Date(b.birthDate))
         .map(elem => {
-            const nameMonth = MONTHS[new Date(elem.birthDay).getMonth()];
+            const nameMonth = MONTHS[new Date(elem.birthDate).getMonth()];
             if (result[nameMonth] === undefined) result[nameMonth] = [];
             result[nameMonth].push(elem.name);
         });
-    console.log(arrStudents);
     return result;
 };
 
 // const arrStudents = [{
 //         name: 'Tom',
-//         birthDay: '05/15/2015',
+//         birthDate: '05/15/2015',
 //     },
 //     {
 //         name: 'Bob',
-//         birthDay: '01/01/2013',
+//         birthDate: '01/01/2013',
 //     },
 //     {
 //         name: 'Jeck',
-//         birthDay: '05/05/2011',
+//         birthDate: '05/05/2011',
 //     },
 //     {
 //         name: 'Anna',
-//         birthDay: '12/19/2008',
+//         birthDate: '12/19/2008',
 //     },
 //     {
 //         name: 'Nick',
-//         birthDay: '01/25/2009',
+//         birthDate: '01/25/2009',
 //     },
 //     {
 //         name: 'Lusy',
-//         birthDay: '12/29/2012',
+//         birthDate: '12/29/2012',
 //     },
 //     {
-//         name: 'Lusy2',
-//         birthDay: '07/29/2012',
+//         name: 'Sam',
+//         birthDate: '07/29/2012',
 //     },
 //     {
-//         name: 'Lusy3',
-//         birthDay: '07/07/2012',
-//     },
-//     {
-//         name: 'Lusy4',
-//         birthDay: '12/28/2012',
-//     },
-//     {
-//         name: 'Lusy5',
-//         birthDay: '11/29/2012',
-//     },
-//     {
-//         name: 'Lusy6',
-//         birthDay: '11/28/2012',
+//         name: 'Ben',
+//         birthDate: '07/07/2012',
 //     },
 // ];
 
