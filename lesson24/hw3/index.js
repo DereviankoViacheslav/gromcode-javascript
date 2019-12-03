93842d98b0cf71af6ea9f9bcb79beae51498aca1
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-03 10:06:02 +0200

    add lesson24 hw3

--- lesson24/hw3/index.js
+++ lesson24/hw3/index.js
@@ -1,39 +1,40 @@
-const tasks = [{
-        id: 0,
-        text: 'Buy milk',
-        done: false,
-        dateCreated: new Date(2018, 7, 1),
-        dateСompleted: undefined,
-    },
-    {
-        id: 4,
-        text: 'Pick up Tom from airport',
-        done: false,
-        dateCreated: new Date(2013, 1, 1),
-        dateСompleted: undefined,
-    },
-    {
-        id: 2,
-        text: 'Visit party',
-        done: false,
-        dateCreated: new Date(2012, 9, 1),
-        dateСompleted: undefined,
-    },
-    {
-        id: 3,
-        text: 'Visit doctor',
-        done: true,
-        dateCreated: new Date(2011, 4, 1),
-        dateСompleted: new Date(2011, 4, 5),
-    },
-    {
-        id: 1,
-        text: 'Buy meat',
-        done: true,
-        dateCreated: new Date(2019, 3, 1),
-        dateСompleted: new Date(2011, 4, 2),
-    },
-];
+// const tasks = [
+//     {
+//         id: 0,
+//         text: 'Buy milk',
+//         done: false,
+//         dateCreated: new Date(2018, 7, 1),
+//         dateСompleted: undefined,
+//     },
+//     {
+//         id: 4,
+//         text: 'Pick up Tom from airport',
+//         done: false,
+//         dateCreated: new Date(2013, 1, 1),
+//         dateСompleted: undefined,
+//     },
+//     {
+//         id: 2,
+//         text: 'Visit party',
+//         done: false,
+//         dateCreated: new Date(2012, 9, 1),
+//         dateСompleted: undefined,
+//     },
+//     {
+//         id: 3,
+//         text: 'Visit doctor',
+//         done: true,
+//         dateCreated: new Date(2011, 4, 1),
+//         dateСompleted: new Date(2011, 4, 5),
+//     },
+//     {
+//         id: 1,
+//         text: 'Buy meat',
+//         done: true,
+//         dateCreated: new Date(2019, 3, 1),
+//         dateСompleted: new Date(2011, 4, 2),
+//     },
+// ];
 
 const tasksList = document.querySelector('.list');
 tasksList.addEventListener('change', taskHandler);
@@ -59,22 +60,22 @@ function renderList(tasksHTML) {
 
 function createListTasksHTML(tasks) {
     return tasks
-    .sort((a, b) => b.dateCreated - a.dateCreated)
-    .sort((a, b) => a.done - b.done)
-    .sort((a, b) => b.dateСompleted - a.dateСompleted)
-    .map(({ id, text, done }) => {
-        const listItem = document.createElement('li');
-        listItem.classList.add('list__item');
-        listItem.setAttribute('id', `${id}`);
-        const chBox = document.createElement('input');
-        chBox.setAttribute('type', 'checkbox');
-        chBox.checked = done;
-        if (done) listItem.classList.add('list__item_done');
-        chBox.classList.add('list__item-checkbox');
-        listItem.append(chBox, text);
+        .sort((a, b) => b.dateCreated - a.dateCreated)
+        .sort((a, b) => a.done - b.done)
+        .sort((a, b) => b.dateСompleted - a.dateСompleted)
+        .map(({ id, text, done }) => {
+            const listItem = document.createElement('li');
+            listItem.classList.add('list__item');
+            listItem.setAttribute('id', `${id}`);
+            const chBox = document.createElement('input');
+            chBox.setAttribute('type', 'checkbox');
+            chBox.checked = done;
+            if (done) listItem.classList.add('list__item_done');
+            chBox.classList.add('list__item-checkbox');
+            listItem.append(chBox, text);
 
-        return listItem;
-    });
+            return listItem;
+        });
 };
 
 function addTask() {
