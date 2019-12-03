69a9e8e3319f34e8154cf30f7437b6c5a209ba77
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-03 08:22:15 +0200

    add lesson24 hw2

--- lesson24/hw2/index.js
+++ lesson24/hw2/index.js
@@ -14,40 +14,4 @@ function studentsBirthDays(students) {
         }, {});
 };
 
-// const arrStudents = [{
-//         name: 'Tom',
-//         birthDate: '05/15/2015',
-//     },
-//     {
-//         name: 'Bob',
-//         birthDate: '01/01/2013',
-//     },
-//     {
-//         name: 'Jeck',
-//         birthDate: '05/05/2011',
-//     },
-//     {
-//         name: 'Anna',
-//         birthDate: '12/19/2008',
-//     },
-//     {
-//         name: 'Nick',
-//         birthDate: '01/25/2009',
-//     },
-//     {
-//         name: 'Lusy',
-//         birthDate: '12/29/2012',
-//     },
-//     {
-//         name: 'Sam',
-//         birthDate: '07/29/2012',
-//     },
-//     {
-//         name: 'Ben',
-//         birthDate: '07/07/2012',
-//     },
-// ];
-
-// console.log(studentsBirthDays(arrStudents));
-
 export { studentsBirthDays };
