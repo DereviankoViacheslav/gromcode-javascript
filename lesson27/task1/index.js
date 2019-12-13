512e32e18beb2808aec2f4c60491e249bcdcd521
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-13 10:16:18 +0200

    add lesson27 task1

---
+++ lesson27/task1/index.js
@@ -0,0 +1,14 @@
+function getLocalStorageData() {
+    return Object.entries(localStorage)
+        .reduce((acc, [key, value]) => {
+            let newValue;
+            try {
+                newValue = JSON.parse(value);
+            } catch (error) {
+                newValue = value;
+            }
+            return {...acc, [key]: value,};
+        }, {});
+}
+
+export { getLocalStorageData };
