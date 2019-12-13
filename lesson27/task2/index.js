a2418787dc4c5a6105288f79da5e3697589f79ba
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-13 10:45:29 +0200

    add lesson27 task2

--- lesson27/task2/index.js
+++ lesson27/task2/index.js
@@ -1,1 +1,18 @@
-console.log('Hello, world!');
+const counter = document.querySelector('.counter');
+const counterValueElement = document.querySelector('.counter__value');
+
+counter.addEventListener('click',counterChange); 
+
+window.addEventListener('storage', (event) => {
+    counterValueElement.textContent = event.newValue;
+});
+
+function counterChange(event) {
+    const button = event.target.classList.contains('counter__button');
+    if (!button) return;
+    let value = +counterValueElement.textContent;
+    const data = event.target.dataset.action;
+    const newValue = data === 'increase' ? value + 1: value - 1;
+    localStorage.setItem('count',newValue);
+    counterValueElement.textContent = newValue;
+};
