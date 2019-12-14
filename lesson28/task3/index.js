3665a1cb48fb75384e637c4e729f5d6ff1613594
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-14 17:52:19 +0200

    add lesson28 task3

--- lesson28/task3/index.js
+++ lesson28/task3/index.js
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+function mult(a) {
+    return function(b) {
+        return a * b;
+    };
+};
+
+function twice(a) {
+    return mult(2)(a);
+};
+
+function triple(a) {
+    return mult(3)(a);
+};
+
+export { twice, triple };
