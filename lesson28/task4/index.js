9d167f9d15ffcede08c1cc02f898958de057c5f6
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-14 18:02:24 +0200

    add lesson28 task4

--- lesson28/task4/index.js
+++ lesson28/task4/index.js
@@ -1,7 +1,11 @@
-function compose(...funcs) {
-    return function(value) {
-        funcs.reduce((acc, func) => func(acc), value);
-    };
-};
+// function compose(...funcs) {
+//     return function(value) {
+//         funcs.reduce((acc, func) => func(acc), value);
+//     };
+// };
 
-export { compose };
+// export { compose };
+
+export const compose = (...funcs) => value => {
+    return funcs.reduce((acc, func) => func(acc), value);
+};
