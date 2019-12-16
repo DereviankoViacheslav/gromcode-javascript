72da364c65ad703f6f89a789b7ed4eb50dcce1b1
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-16 13:53:35 +0200

    add lesson28 hw2

--- lesson28/hw2/index.js
+++ lesson28/hw2/index.js
@@ -1,11 +1,13 @@
 function maxFibonacci(value) {
-    if(value <= 1) return 0;
-    let prev = 0, next = 1;
-    while(next < value) {
+    if(value < 1) return 0;
+    let prev = 1;
+    let next = 1;
+    while(next <= value) {
         let fib = prev + next;
         prev = next;
         next = fib;
-        if(fib >= value) return prev;
+        if(fib === value) return fib;
+        if(fib > value) return prev;
     }
 };
 
