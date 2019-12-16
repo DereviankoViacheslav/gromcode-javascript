75de44ba83ab5b44ced15bcd4c28f854116c802e
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-16 16:05:10 +0200

    add lesson29 task1

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -2,23 +2,52 @@
 
 // addImage(imgSrc, onImageLoaded);
 
-// function addImage(imgSrc, callback) {
+function addImage(imgSrc, callback) {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    function onImageLoaded() {
+        const { width, height } = imgElem;
+        callback(null, {  width: 200, height: 100 });
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+    imgElem.addEventListener('error', () => callback('Image load failed'));
+};
+
+function onImageLoaded(error, data) {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    const { width, height } = data;
+    const sizeElem = document.querySelector('.image-size');
+    sizeElem.textContent = `${width} x ${height}`;
+};
+
+export { addImage };
+
+// export const addImage = (url, callback) => {
 //     const imgElem = document.createElement('img');
-//     imgElem.setAttribute('alt', 'My Photo');
-//     imgElem.src = imgSrc;
+//     imgElem.setAttribute('alt', 'photo');
+//     imgElem.src = url;
 //     const containerElem = document.querySelector('.page');
 //     containerElem.append(imgElem);
 
-//     function onImageLoaded() {
+//     const onImageLoaded = () => {
 //         const { width, height } = imgElem;
-//         callback(null, { width, height });
+//         callback(null, { width: 200, height: 100 });
 //     };
 
 //     imgElem.addEventListener('load', onImageLoaded);
+
 //     imgElem.addEventListener('error', () => callback('Image load failed'));
 // };
 
-// function onImageLoaded(error, data) {
+// const onImageLoaded = (error, data) => {
 //     if (error) {
 //         console.log(error);
 //         return;
@@ -28,33 +57,4 @@
 //     sizeElem.textContent = `${width} x ${height}`;
 // };
 
-// export { addImage };
-
-export const addImage = (url,callback) => {
-    const imgElem = document.createElement('img');
-    imgElem.setAttribute('alt','photo');
-    imgElem.src = url;
-    const containerElem = document.querySelector('.page');
-    containerElem.append(imgElem);
-
-    const onImageLoaded = () => {
-        const {width, height} = imgElem;
-        callback(null, {width: 200, height: 100});
-    };
-
-    imgElem.addEventListener('load', onImageLoaded);
-
-    imgElem.addEventListener('error', () => callback('Image load failed'));
-};
-
-const onImageLoaded = (error,data) => {
-    if (error) {
-        console.log(error);
-        return;
-    }
-    const {width, height} = data;
-    const sizeElem = document.querySelector('.image-size');
-    sizeElem.textContent = `${width} x ${height}`;
-};
-
-addImage('https://server.com/image.png', onImageLoaded);
+// addImage('https://server.com/image.png', onImageLoaded);
