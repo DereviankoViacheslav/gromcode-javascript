3c4d9bb3778c28b2b9bf30ff90fcb3c90ec67392
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-16 16:12:49 +0200

    add lesson29 task3

--- lesson29/task3/index.js
+++ lesson29/task3/index.js
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+function printing() {
+    console.log(1);
+    setTimeout(function() { console.log(5); }, 1000);
+    console.log(2);
+    setTimeout(function() { console.log(4); }, 0);
+    console.log(3);
+};
+
+export { printing };
