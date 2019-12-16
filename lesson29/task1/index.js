9891f574c61fc53aef335d0a93ec249cd681f6c0
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-16 16:07:15 +0200

    add lesson29 task1

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -10,8 +10,8 @@ function addImage(imgSrc, callback) {
     containerElem.append(imgElem);
 
     function onImageLoaded() {
-        const { width, height } = imgElem;
-        callback(null, {  width: 200, height: 100 });
+        const {  width = 200, height = 100 } = imgElem;
+        callback(null, {  width, height });
     };
 
     imgElem.addEventListener('load', onImageLoaded);
@@ -29,32 +29,3 @@ function onImageLoaded(error, data) {
 };
 
 export { addImage };
-
-// export const addImage = (url, callback) => {
-//     const imgElem = document.createElement('img');
-//     imgElem.setAttribute('alt', 'photo');
-//     imgElem.src = url;
-//     const containerElem = document.querySelector('.page');
-//     containerElem.append(imgElem);
-
-//     const onImageLoaded = () => {
-//         const { width, height } = imgElem;
-//         callback(null, { width: 200, height: 100 });
-//     };
-
-//     imgElem.addEventListener('load', onImageLoaded);
-
-//     imgElem.addEventListener('error', () => callback('Image load failed'));
-// };
-
-// const onImageLoaded = (error, data) => {
-//     if (error) {
-//         console.log(error);
-//         return;
-//     }
-//     const { width, height } = data;
-//     const sizeElem = document.querySelector('.image-size');
-//     sizeElem.textContent = `${width} x ${height}`;
-// };
-
-// addImage('https://server.com/image.png', onImageLoaded);
