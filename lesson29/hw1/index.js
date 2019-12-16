3aa9a2f763475a208da51b8e67ae28660bd4945f
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-16 17:02:45 +0200

    add lesson29 hw1

--- lesson29/hw1/index.js
+++ lesson29/hw1/index.js
@@ -2,14 +2,14 @@ function requestUserData(userId, callback) {
     setTimeout(() => {
         if (userId === 'broken') {
             callback(null, 'Failed to load user data');
-        } else {
-            callback({
-                name: 'John',
-                age: 17,
-                userId,
-                email: 'userid777@example.com',
-            });
+            return;
         }
+        callback({
+            name: 'John',
+            age: 17,
+            userId: userId,
+            email: 'userid777@example.com',
+        });
     }, Math.floor(Math.random() * 3 + 1) * 1000);
 };
 
