002738dd721491b4f8d4ac803b4c23e21fb5a35a
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-16 15:57:21 +0200

    add lesson29 task2

--- lesson29/task2/index.js
+++ lesson29/task2/index.js
@@ -2,12 +2,13 @@ function pinger(num, period) {
     let i = num;
     console.log('Ping');
     const idInterval = setInterval(() => {
-        if (i-- > 0) {
+        if (--i > 0) {
             console.log('Ping');
         } else {
             clearInterval(idInterval);
         }
     }, period)
 };
+pinger(5, 1000);
 
 export { pinger };
