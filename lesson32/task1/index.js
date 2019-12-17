cb146b6032e35b10cae239af5dd024b587025383
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-17 17:41:45 +0200

    add lesson32 task1

--- lesson32/task1/index.js
+++ lesson32/task1/index.js
@@ -1,3 +1,21 @@
+// const asyncNumber1 = getValueWithDelay(24, 1000);
+// const asyncNumber2 = getValueWithDelay(undefined, 2000);
+// const asyncNumber3 = Promise.reject(new Error('err'));
+// const asyncNumber4 = getValueWithDelay('15', 4000);
+// const asyncNumber5 = getValueWithDelay(10, 5000);
+
+// asyncSum(asyncNumber1, asyncNumber2, asyncNumber3, asyncNumber4, asyncNumber5)
+//     .then(result => console.log(result));
+
+// function getValueWithDelay(value, delay) {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             console.log(value);
+//             resolve(value)
+//         }, delay);
+//     });
+// };
+
 function asyncSum(...asyncNumbers) {
     return Promise.all(asyncNumbers)
         .then(numbers => getSum(numbers))
@@ -7,7 +25,7 @@ function asyncSum(...asyncNumbers) {
 function getSum(numbers) {
     return numbers
         .filter(elem => !isNaN(elem))
-        .reduce((acc, elem) => +elem + acc, 0);
+        .reduce((acc, elem) => acc + Number(elem), 0);
 };
 
 export { asyncSum };
