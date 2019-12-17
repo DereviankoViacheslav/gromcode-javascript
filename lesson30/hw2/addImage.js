280c0776ab1e4afd5f195c1a0c0cd1b8c16ac97b
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-17 10:33:48 +0200

    add lesson30 hw2

---
+++ lesson30/hw2/addImage.js
@@ -0,0 +1,17 @@
+function addImage(imgSrc, callback) {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    function onImageLoaded() {
+        const {  width, height } = imgElem;
+        callback(null, {  width, height });
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+    imgElem.addEventListener('error', () => callback('Image load failed'));
+};
+
+export { addImage };
--- lesson30/hw2/index.js
+++ lesson30/hw2/index.js
@@ -1,1 +1,19 @@
-console.log('Hello, world!');
+import { addImage } from './addImage.js';
+
+function addImageV2(imgSrc) {
+    return new Promise((resolve, reject) => {
+        addImage(imgSrc, (error, data) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(data);
+        })
+    });
+};
+
+// addImageV2('https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg')
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
+
+export { addImageV2 };
