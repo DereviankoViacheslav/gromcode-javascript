7884992badcc33dbce8ca506d196ea4d12cd8bb8
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-17 18:27:57 +0200

    add lesson32 task2

--- lesson32/task2/index.js
+++ lesson32/task2/index.js
@@ -1,1 +1,30 @@
-console.log('Hello, world!');
+const servers = [
+    'https://server.com/us/',
+    'https://server.com/eu/',
+    'https://server.com/au/'
+];
+
+function request(url) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                userData: {
+                    name: 'Tom',
+                    age: 17,
+                },
+                source: url,
+            });
+        }, (1000 + Math.random() * (3000 - 1000)));
+    });
+};
+
+function getUserASAP(userId) {
+    const userUrls = servers.map(serverUrl => `${serverUrl}/${userId}`);
+    const requests = userUrls.map(userUrl => request(userUrl));
+
+    return Promise.race(requests);
+};
+
+// getUserASAP('7777').then(value => console.log(value));
+
+export { getUserASAP };
