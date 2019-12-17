ee39f341b4c57cd6de25cae0e0300c7287f3d1e8
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-17 13:00:10 +0200

    add lesson31 task2

--- lesson31/task2/index.js
+++ lesson31/task2/index.js
@@ -1,1 +1,24 @@
-console.log('Hello, world!');
+function asyncCalculator(numbr) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`Initial value: ${numbr}`);
+            resolve(numbr);
+        }, 500);
+    })
+    .then(value => new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const result = value * value;
+            console.log(`Squared value: ${result}`);
+            resolve(result);
+        }, 500);
+    }))
+    .then(value => new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const result = value * 2;
+            console.log(`Doubled value: ${result}`);
+            resolve(result);
+        }, 500);
+    }))
+};
+
+export { asyncCalculator };
