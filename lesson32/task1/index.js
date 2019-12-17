9c30cdc64078bcd08a22655e6f1718a3dd55e052
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-17 17:43:09 +0200

    add lesson32 task1

--- lesson32/task1/index.js
+++ lesson32/task1/index.js
@@ -1,25 +1,25 @@
-// const asyncNumber1 = getValueWithDelay(24, 1000);
-// const asyncNumber2 = getValueWithDelay(undefined, 2000);
-// const asyncNumber3 = Promise.reject(new Error('err'));
-// const asyncNumber4 = getValueWithDelay('15', 4000);
-// const asyncNumber5 = getValueWithDelay(10, 5000);
+const asyncNumber1 = getValueWithDelay(24, 1000);
+const asyncNumber2 = getValueWithDelay(undefined, 2000);
+const asyncNumber3 = Promise.reject(new Error('err'));
+const asyncNumber4 = getValueWithDelay('15', 4000);
+const asyncNumber5 = getValueWithDelay(10, 5000);
 
-// asyncSum(asyncNumber1, asyncNumber2, asyncNumber3, asyncNumber4, asyncNumber5)
-//     .then(result => console.log(result));
+asyncSum(asyncNumber1, asyncNumber2, asyncNumber3, asyncNumber4, asyncNumber5)
+    .then(result => console.log(result));
 
-// function getValueWithDelay(value, delay) {
-//     return new Promise(resolve => {
-//         setTimeout(() => {
-//             console.log(value);
-//             resolve(value)
-//         }, delay);
-//     });
-// };
+function getValueWithDelay(value, delay) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log(value);
+            resolve(value)
+        }, delay);
+    });
+};
 
 function asyncSum(...asyncNumbers) {
     return Promise.all(asyncNumbers)
         .then(numbers => getSum(numbers))
-        .catch(() => Promise.reject(new Error(`Can't calculate`)));
+        .catch(() => Promise.reject(new Error('Can\'t calculate')));
 };
 
 function getSum(numbers) {
