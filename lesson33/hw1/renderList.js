6e687dbfd9cdb728c8b54d4e8a7011edd1bc0fe1
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-19 16:14:19 +0200

    add lesson33 hw1

--- lesson33/hw1/renderList.js
+++ lesson33/hw1/renderList.js
@@ -6,43 +6,18 @@ const inputAmountDaysElem = document.querySelector('.form__input-amount-days');
 const usersListElem = document.querySelector('.users-list');
 const showUserBtnElem = document.querySelector('.form__btn');
 
-showUserBtnElem.addEventListener('click', onSearchMostActiveDevs);
+function renderList() {
+    showUserBtnElem.addEventListener('click', onSearchMostActiveDevs);
+};
 
 function onSearchMostActiveDevs() {
     const userId = inputNameOwnerElem.value;
     const repoId = inputNameRepoElem.value;
-    const days = inputAmountDaysElem.value;
+    // const days = inputAmountDaysElem.value;
+    const days = 7;
+    
     getMostActiveDevs({ days, userId, repoId })
         .then(users => renderUsers(users));
-};
-
-function getDevelopers(commits, days) {
-    const fromDate = getStartDate(days);
-
-    const dataUsers = commits
-        .filter(commit => new Date(commit.commit.author.date) - fromDate > 0)
-        .reduce((acc, commit) => {
-            const id = commit.author.id;
-            acc[id] = acc[id] ? acc[id] : {};
-            const avatar = acc[id].avatar ? acc[id].avatar : commit.author.avatar_url;
-            const name = acc[id].name ? acc[id].name : commit.commit.author.name;
-            const email = acc[id].email ? acc[id].email : commit.commit.author.email;
-            const count = acc[id].count ? ++acc[id].count : 1;
-
-            return { ...acc, [id]: { name, email, count } };
-            // return { ...acc, [id]: { id, name, email, avatar, count } };
-        }, {});
-
-    const arrUsers = Object.values(dataUsers)
-        .sort((a, b) => b.count - a.count);
-    const commitsTopDeveloper = arrUsers[0].count;
-
-    return arrUsers.filter(({ count }) => commitsTopDeveloper === count);
-};
-
-function getStartDate(daysAgo) {
-    const now = new Date();
-    return new Date(now.setDate(now.getDate() - daysAgo));
 };
 
 function renderUsers(users) {
@@ -58,4 +33,4 @@ function renderUsers(users) {
     usersListElem.innerHTML = listItem;
 };
 
-export { getDevelopers };
+export { renderList };
