c1e6382209c0373ac97841cd6712558b7f9ecd7e
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-19 15:57:47 +0200

    add lesson33 hw1

--- lesson33/hw1/index.js
+++ lesson33/hw1/index.js
@@ -1,18 +1,4 @@
-const inputNameOwnerElem = document.querySelector('.form__input-name-owner');
-const inputNameRepoElem = document.querySelector('.form__input-name-repo');
-const inputAmountDaysElem = document.querySelector('.form__input-amount-days');
-const usersListElem = document.querySelector('.users-list');
-const showUserBtnElem = document.querySelector('.form__btn');
-
-showUserBtnElem.addEventListener('click', onSearchMostActiveDevs);
-
-function onSearchMostActiveDevs() {
-    const userId = inputNameOwnerElem.value;
-    const repoId = inputNameRepoElem.value;
-    const days = inputAmountDaysElem.value;
-    getMostActiveDevs({ days, userId, repoId })
-        .then(users => renderUsers(users));
-};
+import { getDevelopers } from './renderList.js';
 
 // `https://api.github.com/repos/DereviankoViacheslav/calendar-web-app/commits?per_page=100`)
 
@@ -20,48 +6,6 @@ function getMostActiveDevs({ days, userId, repoId }) {
     return fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
         .then(respons => respons.json())
         .then(commits => getDevelopers(commits, days));
-};
-
-function getDevelopers(commits, days) {
-    const fromDate = getStartDate(days);
-
-    const dataUsers = commits
-        .filter(commit => new Date(commit.commit.author.date) - fromDate > 0)
-        .reduce((acc, commit) => {
-            const id = commit.author.id;
-            acc[id] = acc[id] ? acc[id] : {};
-            const avatar = acc[id].avatar ? acc[id].avatar : commit.author.avatar_url;
-            const name = acc[id].name ? acc[id].name : commit.commit.author.name;
-            const email = acc[id].email ? acc[id].email : commit.commit.author.email;
-            const count = acc[id].count ? ++acc[id].count : 1;
-
-            // return { ...acc, [id]: { id, name, email, avatar, count } };
-            return { ...acc, [id]: { name, email, count } };
-        }, {});
-
-    const arrUsers = Object.values(dataUsers)
-        .sort((a, b) => b.count - a.count);
-    const commitsTopDeveloper = arrUsers[0].count;
-
-    return arrUsers.filter(({ count }) => commitsTopDeveloper === count);
-};
-
-function getStartDate(daysAgo) {
-    const now = new Date();
-    return new Date(now.setDate(now.getDate() - daysAgo));
-};
-
-function renderUsers(users) {
-    let listItem = '';
-
-    users.map(({ name }) => {
-        // users.map(({ name, avatar }) => {
-        // <img class="user__avatar" src="${avatar}" alt="User Avatar">
-        listItem += `<li class="users-list__item">
-                        <span class="user__name">${name}</span>
-                    </li>`;
-    });
-    usersListElem.innerHTML = listItem;
 };
 
 export { getMostActiveDevs };
