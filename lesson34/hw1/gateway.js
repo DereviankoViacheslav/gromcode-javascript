de10953f93610307b715ef599319b972afd30b5e
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-23 10:30:40 +0200

    add lesson34 hw1

--- lesson34/hw1/gateway.js
+++ lesson34/hw1/gateway.js
@@ -1,13 +1,13 @@
-const baseUrl = 'https://crudcrud.com/api/6292d9d47ada4bd4afc3cd42969fdea2/users';
+// const baseUrl = 'https://crudcrud.com/api/6292d9d47ada4bd4afc3cd42969fdea2/users';
 
-function addUser(userData) {
-    return fetch(baseUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-        },
-        body: JSON.stringify(userData),
-    })
-};
+// function addUser(userData) {
+//     return fetch(baseUrl, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json;charset=utf-8',
+//         },
+//         body: JSON.stringify(userData),
+//     })
+// };
 
-export { addUser };
+// export { addUser };
--- lesson34/hw1/index.js
+++ lesson34/hw1/index.js
@@ -1,5 +1,59 @@
-import { initHandlers } from './initHandlers.js';
+// import { initHandlers } from './initHandlers.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    initHandlers();
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     initHandlers();
+// });
+
+const baseUrl = 'https://crudcrud.com/api/6292d9d47ada4bd4afc3cd42969fdea2/users';
+
+function addUser(userData) {
+    return fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(userData),
+    })
+};
+
+const submitBtnElem = document.querySelector('.submit-button');
+const form = document.forms[0];
+const errorTextElem = document.querySelector('.error-text');
+const inputEmailElems = document.querySelector('#email');
+const inputNameElems = document.querySelector('#name');
+const inputPasswordElems = document.querySelector('#password');
+
+form.addEventListener('input', validateFields);
+form.addEventListener('submit', submitData);
+// function initHandlers() {
+// };
+
+function validateFields() {
+    if (form.reportValidity()) {
+        submitBtnElem.disabled = false;
+    } else {
+        submitBtnElem.disabled = true;
+    }
+    errorTextElem.textContent = '';
+};
+
+function submitData(event) {
+    event.preventDefault();
+    const newUser = {
+        name: inputNameElems.value,
+        email: inputEmailElems.value,
+        password: inputPasswordElems.value,
+    };
+
+    addUser(newUser)
+        .then(data => {
+            inputNameElems.value = '';
+            inputEmailElems.value = '';
+            inputPasswordElems.value = '';
+            submitBtnElem.disabled = true;
+            alert(data);
+        })
+        .catch(() => {
+            errorTextElem.textContent = 'Failed to create user';
+        });
+};
