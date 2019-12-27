9201ee04554f2cf5ab0630af5b21494a1a05a7e8
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-27 12:44:50 +0200

    add lesson35 task3

--- lesson35/task3/index.js
+++ lesson35/task3/index.js
@@ -1,1 +1,37 @@
-console.log('Hello, world!');
+import { fetchUserData, fetchRepositories } from './gateways.js'
+import { renderUserData } from './user.js'
+import { renderRepos, cleanReposList } from './repos.js'
+import { showSpinner, hideSpinner } from './spinner.js'
+
+const defaultUser = {
+    avatar_url: `https://avatars3.githubusercontent.com/u10001`,
+    name: '',
+    location: '',
+};
+
+renderUserData(defaultUser);
+
+const showUserBtnElem = document.querySelector('.name-form__btn');
+const userNameInputElem = document.querySelector('.name-form__input');
+
+function onSearchUser() {
+    showSpinner();
+    cleanReposList();
+    const userName = userNameInputElem.value;
+    fetchUserData(userName)
+        .then(userData => {
+            renderUserData(userData);
+            return userData.repos_url;
+        })
+        .then(url => fetchRepositories(url))
+        .then(reposList => {
+            hideSpinner();
+            renderRepos(reposList);
+        })
+        .catch(err => {
+            hideSpinner();
+            alert(err.message);
+        })
+};
+
+showUserBtnElem.addEventListener('click', onSearchUser);
