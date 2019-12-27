f84b90054e4550f1f5be2a71214ef61f53e36169
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-27 13:11:41 +0200

    add lesson35 task3

--- lesson35/task3/index.js
+++ lesson35/task3/index.js
@@ -1,7 +1,7 @@
-import { fetchUserData, fetchRepositories } from './gateways.js'
-import { renderUserData } from './user.js'
-import { renderRepos, cleanReposList } from './repos.js'
-import { showSpinner, hideSpinner } from './spinner.js'
+import { fetchUserData, fetchRepositories } from './src/js/gateways.js'
+import { renderUserData } from './src/js/user.js'
+import { renderRepos, cleanReposList } from './src/js/repos.js'
+import { showSpinner, hideSpinner } from './src/js/spinner.js'
 
 const defaultUser = {
     avatar_url: `https://avatars3.githubusercontent.com/u10001`,
