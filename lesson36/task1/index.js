6dce25921614012c54017edc5a9a77aa5907c069
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-27 16:17:33 +0200

    add lesson36 task1

---
+++ lesson36/task1/index.js
@@ -0,0 +1,13 @@
+async function fetchUser(userId) {
+    try {
+        const response = await fetch(`https://api.github.com/users/${userId}`)
+        if (!response.ok) return null;
+
+        const UserData = await response.json()
+        return UserData;
+    } catch (e) {
+        throw new Error('Failed to fetch user')
+    }
+}
+
+export { fetchUser };
