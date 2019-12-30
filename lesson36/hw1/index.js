52d68ffa476c482dacd94a749dd7451aa0189529
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-30 11:32:33 +0200

    add lesson36 hw1

--- lesson36/hw1/index.js
+++ lesson36/hw1/index.js
@@ -1,18 +1,27 @@
 async function getUsersBlogs(usersList) {
-    try {
-        const requests = usersList
-            .map(userId => fetch(`https://api.github.com/users/${userId}`)
-                .then(response => response.json()));
-        const usersData = await Promise.all(requests);
-        return usersData.map(user => user.blog);
-    } catch (error) {
-        throw new Error(error.massage);
-    }
+    // try {
+    const requests = usersList
+        .map(userId => fetch(`https://api.github.com/users/${userId}`)
+            .then(response => {
+                if (response.ok) return response.json();
+                throw new Error(error.massage);
+            }));
+    const usersData = await Promise.all(requests);
+    return usersData.map(user => user.blog);
+    // } catch (error) {
+    //     throw new Error(error.massage);
+    // }
 };
 
-// getUsersBlogs(['facebook8989111', 'github'])
-// getUsersBlogs(['facebook', 'google', 'gaearon'])
-getUsersBlogs(['facebook89899998', 'facebook', 'google', 'gaearon'])
+// getUsersBlogs(['facebook89899998', 'facebook', 'google', 'gaearon'])
+getUsersBlogs(['facebook', 'google', 'gaearon'])
     .then(result => console.log(result));
 
 export { getUsersBlogs };
+
+
+/*
+    .catch(error => {
+        throw new Error(error.massage);
+    })
+*/
