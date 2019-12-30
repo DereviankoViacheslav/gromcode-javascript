6397ca65a2c06319e8c7b54cd12fe3a510d6c75e
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-30 08:31:16 +0200

    add lesson36 hw1

--- lesson36/hw1/index.js
+++ lesson36/hw1/index.js
@@ -3,19 +3,17 @@ async function getUsersBlogs(users) {
     try {
         for (let i = 0; i < users.length; i++) {
             const response = await fetch(`https://api.github.com/users/${users[i]}`);
-            // if (!response.ok) throw new Error('Failed to load data');
+            if (!response.ok) throw new Error('Failed to load data');
             const result = await response.json()
             userBlogs.push(result.blog);
         }
-        return await userBlogs;
+        return userBlogs;
     } catch (error) {
         console.log(error);
     }
-
-    // return await userBlogs;
 };
 
-getUsersBlogs(['faceboo89899998k', 'facebook', 'google', 'gaearon'])
+getUsersBlogs(['facebook89899998', 'facebook', 'google', 'gaearon'])
     .then(result => console.log(result));
 
 export { getUsersBlogs };
