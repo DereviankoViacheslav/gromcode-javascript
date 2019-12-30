c384616572c80ef39d74423d164baf6cba12a992
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-12-30 11:04:33 +0200

    add lesson36 hw1

--- lesson36/hw1/index.js
+++ lesson36/hw1/index.js
@@ -1,29 +1,20 @@
 async function getUsersBlogs(usersList) {
-    // const userBlogs = [];
-    // try {
-    //     for (let i = 0; i < users.length; i++) {
-    //         const response = await fetch(`https://api.github.com/users/${users[i]}`);
-    //         // if (!response.ok) throw new Error('Failed to load data');
-    //         const result = await response.json()
-    //         userBlogs.push(result.blog);
-    //     }
-    //     return userBlogs;
-    // } catch (error) {
-    //     console.log(error);
-    // }
-    const requests = usersList
-        .map(userId => fetch(`https://api.github.com/users/${userId}`)
-            .then(response => response.json()));
-    // return await Promise.all(requests);
-    const usersData = await Promise.all(requests);
-    return usersData.map(user => user.blog);
+    try {
+        const requests = usersList
+            .map(userId => fetch(`https://api.github.com/users/${userId}`)
+                .then(response => response.json())
+                .catch(error => {
+                    throw new Error(error.massage);
+                }));
+        const usersData = await Promise.all(requests);
+        return usersData.map(user => user.blog);
+    } catch (error) {
+        throw new Error(error.massage);
+    }
 };
 
+// getUsersBlogs(['facebook', 'google', 'gaearon'])
 getUsersBlogs(['facebook89899998', 'facebook', 'google', 'gaearon'])
     .then(result => console.log(result));
 
 export { getUsersBlogs };
-
-// const requests = usersList
-//             .map(userId => fetch(`https://api.github.com/users/${userId}`).then(response => response.json()));
-//         const usersData = await Promise.all(requests);
